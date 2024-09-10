7dcc098c86a3bddfc0f4c5fa4a0a7f495e336d37
KimJiTae <wlxo0401@gmail.com>
2024-09-10 18:31:09 +0900

    주석

--- ItemHorizontalScroll/ItemHorizontalScroll/Custom/ItemView.swift
+++ ItemHorizontalScroll/ItemHorizontalScroll/Custom/ItemView.swift
@@ -8,16 +8,20 @@
 import UIKit
 
 final class ItemView: UIView {
+    // Label to display the item text
     private lazy var label: UILabel = {
         let label = UILabel()
         label.textAlignment = .center
         return label
     }()
     
+    // Button to handle user interaction
     internal lazy var button: UIButton = {
         let button = UIButton()
         return button
     }()
+    
+    // MARK: - Initialization
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -28,24 +32,30 @@ final class ItemView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
+    // Set up the UI components
     private func initUI() {
         self.addSubview(self.label)
         self.addSubview(self.button)
         
+        // Configure constraints for label
         self.label.snp.makeConstraints { make in
             make.top.bottom.equalToSuperview().inset(6)
             make.leading.trailing.equalToSuperview().inset(4)
         }
+        
+        // Configure constraints for button
         self.button.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
     }
     
+    // Configure the item view with text and selection state
     func configure(with text: String, isSelected: Bool) {
         self.label.text = text
         self.selectItem(isSelected: isSelected)
     }
-        
+    
+    // Update the visual state based on selection
     func selectItem(isSelected: Bool) {
         self.label.textColor = isSelected ? .black : .gray
     }
